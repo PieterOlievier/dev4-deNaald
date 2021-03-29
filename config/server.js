33bab84c0b5475498f8e52e30ff2424f0691fd90
Pieter Olieiver <pieter.olievier@student.howest.be>
2021-03-29 11:08:08 +0200

    eerste commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9dad3510b10890b13b4e71aa44d59cfc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9a03f22e-072a-4e22-a719-9c3427a50d5e'
+};
